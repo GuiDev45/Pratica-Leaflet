e7af2c9e49e60d9c277766e7316c7a259e04acee
GuiDev45 <bortolettodev@gmail.com>
2023-08-13 17:52:12 -0300

    Configuração inicial e organização de arquivos - COMENTADO

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import './App.css'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-// Lembrando que sem o CSS do leaflet, o mapa vai bugar
+// importa o arquivo de estilo CSS do pacote Leaflet, que é a biblioteca subjacente usada pelo react-leaflet para renderizar mapas. É importante importar esse arquivo de estilo para que o mapa seja exibido corretamente.
 import "leaflet/dist/leaflet.css";
 
+// Aqui começa a definição do componente App. É uma função React que retorna a estrutura do componente.
 function App() {
   return (
     <div>
+      {/* Inicia um componente MapContainer que representa um contêiner para o mapa interativo. Ele possui duas propriedades: center, que define a coordenada do centro do mapa [latitude e longitude], e zoom, que define o nível de zoom inicial do mapa.*/}
       <MapContainer center={[51.505, -0.09]} zoom={13}>
+        {/* Este componente TileLayer representa uma camada de mosaico do mapa. Ele recebe uma URL para os mosaicos do mapa a serem exibidos, bem como atribuições de direitos autorais.*/}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {/* Marker representa um marcador que será exibido no mapa. Ele recebe a propriedade position, que define a coordenada onde o marcador será colocado.*/}
         <Marker position={[51.505, -0.09]}>
+          {/* componente Popup é usado para criar uma caixa de diálogo pop-up associada a um marcador. Ele contém texto que será exibido no pop-up, neste caso, "Esse é um popup" e "Descrição do popup."*/}
           <Popup>
             Esse é um popup <br /> Descrição do popup.
           </Popup>
@@ -21,4 +26,5 @@ function App() {
   )
 }
 
+// Exporta o componente App como o componente padrão deste módulo, o que permite que ele seja usado em outros lugares do código.
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+// biblioteca React, que é usada para criar interfaces de usuário com componentes reutilizáveis e gerenciar o estado da aplicação.
 import React from "react";
+// importa o módulo ReactDOM, que é uma parte do React usada para renderizar componentes React em um ambiente de navegador.
 import ReactDOM from "react-dom";
+// importa o componente App
 import App from "./App";
 
+// Aqui começa o processo de renderização do React, que envolve a renderização do componente principal (App) na página HTML.
 ReactDOM.render(
+   // componente especial do React chamado StrictMode. Ele envolve a aplicação e ajuda a identificar potenciais problemas e práticas desatualizadas durante o desenvolvimento, exibindo avisos no console. Isso não afeta a produção e é destinado apenas ao desenvolvimento.
    <React.StrictMode>
+     {/* o componente App é renderizado dentro do StrictMode. Isso significa que todo o conteúdo do componente App será renderizado no lugar onde ele é colocado.*/}
      <App />
    </React.StrictMode>,
+   // seleciona o elemento HTML com o id "root" na página HTML, é onde o aplicativo React é renderizado.
    document.getElementById("root"),
  );
